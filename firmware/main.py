e43e04329687cfecef535daca89c337c5de939e7
HydrogenMacro <hydrogenmacro@gmail.com>
2026-02-18 19:44:45 -0500

    make main.py work

--- firmware/main.py
+++ firmware/main.py
@@ -6,17 +6,20 @@ from kmk.kmk_keyboard import KMKKeyboard
 from kmk.scanners.keypad import KeysScanner
 from kmk.keys import KC
 from kmk.modules.macros import Press, Release, Tap, Macros
-from kmk.extensions.RGB import RGB
+from kmk.extensions.rgb import RGB, AnimationModes
+from kmk.extensions.media_keys import MediaKeys
 
 # This is the main instance of your keyboard
 keyboard = KMKKeyboard()
+
+keyboard.extensions.append(MediaKeys())
 
 # Add the macro extension
 macros = Macros()
 keyboard.modules.append(macros)
 
 # Define your pins here!
-PINS = [board.D1, board.D2, board.D4, board.D3, board.D7, board.D6]
+PINS = [board.D7, board.D8, board.D9, board.D10, board.D5, board.D4]
 
 # Tell kmk we are not using a key matrix
 keyboard.matrix = KeysScanner(
@@ -34,31 +37,33 @@ keyboard.keymap = [
 ]
 
 # osu! keybinds
-"""
-keyboard.keymap = [
-    [KC.ENTER, KC.Z, KC.X, KC.Z, KC.ESCAPE, KC.ESCAPE,]
-]
-"""
 
-# Ok I dont have the board in front of me, so who knows if this code works or not
+keyboard.keymap = [
+    [KC.SPACE, KC.Z, KC.X, KC.C, KC.ESCAPE, KC.ESCAPE,]
+]
+
+# geometry dash keybinds
+
+keyboard.keymap = [
+    [KC.ESCAPE, KC.SPACE, KC.SPACE, KC.SPACE, KC.ESCAPE,]
+]
+
 
 # Encoder support
 from kmk.modules.encoder import EncoderHandler
 encoder_handler = EncoderHandler()
 keyboard.modules.append(encoder_handler)
+
 encoder_handler.pins = (
-    (board.GP27, board.GP28, board.GP26,),
+    (board.D1, board.D2, board.D0, False),
 )
 encoder_handler.map = [
-    ((KC.VOLD, KC.VOLU, KC.MUTE),),
+    ((KC.AUDIO_VOL_DOWN, KC.AUDIO_VOL_UP, KC.AUDIO_MUTE),),
 ]
 
-# RGB colors (currently hard white, will tinker with once I have the board)
-rgb = RGB(pixel_pin=board.D29, num_pixels=6, rgb_order=(1, 0, 2))
-rgb.set_rgb_fill(255, 255, 255)
+rgb = RGB(pixel_pin=board.D3, num_pixels=6, animation_mode = AnimationModes.BREATHING, sat_default = 0, breathe_center = 2, animation_speed = 5)
 keyboard.extensions.append(rgb)
 
-
-# Start kmk!
 if __name__ == '__main__':
+    print("Starting KMK:");
     keyboard.go()
